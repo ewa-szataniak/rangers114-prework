0ea8657f3f05f3ba4663e3570c5fb9ffa71afae1
ewa-szataniak <ewamariacoach@gmail.com>
2023-03-13 15:46:21 -0400

    Initial commit

---
+++ python.py
@@ -0,0 +1,41 @@
+
+#Question 1 #
+
+def hello_name(username):
+ print ("Hello, " + username.title() + "!" )
+hello_username="Hello, Elliot!"
+hello_name('Elliot')
+
+def get_user(username):
+ print("Hello, " + username.title() +"!")
+
+ get_user("Elliot")
+
+ def hello_name(user_name):
+  """a function to print hello_USERNAME"""
+  print("hello, " + user_name.title()+"!")
+
+
+# Question 2- Print first odd numbers between 1 and 100
+def first_odds():
+ #print all odd numbers from 1 to 100#
+ number = 1
+ while number <101:
+  if number %2==1:
+   print(number)
+   number +=1
+
+   def odd_number():
+    num= list(range(1, 100))
+    for no in num:
+     if no % 2 !=0:
+      print(no)
+
+      
+    
+
+
+
+
+
+
